2cd1f4dcfb6ceb7b6933e0861ebfc5cd584985f4
CalinaThalya <calina.thalya@icloud.com>
2024-06-06 20:48:29 -0300

    CRUD VAZAMENTO

--- components/Vazamentos.js
+++ components/Vazamentos.js
@@ -1,15 +1,122 @@
-import React from 'react';
-import { View, ImageBackground, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, ImageBackground, Text, StyleSheet, TouchableOpacity, TextInput, Button, FlatList } from 'react-native';
+import axios from 'axios';
 
 const Vazamentos = ({ navigation }) => {
+  const [vazamentos, setVazamentos] = useState([]);
+  const [newVazamento, setNewVazamento] = useState({ title: '', description: '' });
+  const [updatingVazamento, setUpdatingVazamento] = useState(null);
+  const [editedTitle, setEditedTitle] = useState('');
+  const [editedDescription, setEditedDescription] = useState('');
+
+  useEffect(() => {
+    fetchVazamentos();
+  }, []);
+
+  const fetchVazamentos = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/vazamentos');
+      setVazamentos(response.data);
+    } catch (error) {
+      console.error('Erro ao obter os vazamentos:', error);
+    }
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.vazamentoItem}>
+      <Text style={styles.vazamentoTitle}>{item.title}</Text>
+      <Text style={styles.vazamentoDescription}>{item.description}</Text>
+      <TouchableOpacity onPress={() => handleDeleteVazamento(item.id)}>
+        <Text style={styles.deleteButton}>Deletar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => handleEditVazamento(item)}>
+        <Text style={styles.editButton}>Editar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  const handleAddVazamento = async () => {
+    try {
+      await axios.post('http://localhost:3000/vazamentos', newVazamento);
+      fetchVazamentos();
+      setNewVazamento({ title: '', description: '' });
+    } catch (error) {
+      console.error('Erro ao adicionar o vazamento:', error);
+    }
+  };
+
+  const handleEditVazamento = (item) => {
+    setUpdatingVazamento(item);
+    setEditedTitle(item.title);
+    setEditedDescription(item.description);
+  };
+
+  const handleSaveEditedVazamento = async () => {
+    try {
+      await axios.put(`http://localhost:3000/vazamentos/${updatingVazamento.id}`, {
+        title: editedTitle,
+        description: editedDescription
+      });
+      fetchVazamentos();
+      setUpdatingVazamento(null);
+      setEditedTitle('');
+      setEditedDescription('');
+    } catch (error) {
+      console.error('Erro ao editar o vazamento:', error);
+    }
+  };
+
+  const handleDeleteVazamento = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3000/vazamentos/${id}`);
+      fetchVazamentos();
+    } catch (error) {
+      console.error('Erro ao deletar o vazamento:', error);
+    }
+  };
 
   return (
     <View style={styles.container}>
       <ImageBackground source={require('../assets/74ac2c024258adaa4088487bfc12214c.jpg')} style={styles.backgroundImage}>
         <View style={styles.overlay}>
-        
           <Text style={styles.title}>Vazamentos</Text>
-          <Text style={styles.topic}>API DA IA </Text>
+          <View style={styles.formContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Título"
+              value={newVazamento.title}
+              onChangeText={(text) => setNewVazamento({ ...newVazamento, title: text })}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Descrição"
+              value={newVazamento.description}
+              onChangeText={(text) => setNewVazamento({ ...newVazamento, description: text })}
+            />
+            <Button title="Adicionar Vazamento" onPress={handleAddVazamento} />
+          </View>
+          {updatingVazamento && (
+            <View style={styles.formContainer}>
+              <TextInput
+                style={styles.input}
+                placeholder="Título"
+                value={editedTitle}
+                onChangeText={(text) => setEditedTitle(text)}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Descrição"
+                value={editedDescription}
+                onChangeText={(text) => setEditedDescription(text)}
+              />
+              <Button title="Salvar Edição" onPress={handleSaveEditedVazamento} />
+            </View>
+          )}
+          <FlatList
+            data={vazamentos}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id.toString()}
+          />
         </View>
       </ImageBackground>
       <View style={styles.footer}>
@@ -43,30 +150,39 @@ const styles = StyleSheet.create({
     fontSize: 30,
     marginBottom: 20,
   },
-
-  footer: {
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black',
-    paddingVertical: 10,
+  vazamentoItem: {
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
   },
-  footerText: {
-    color: '#FFF',
-    fontSize: 14,
+  vazamentoTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 5,
   },
-  text: {
-    color: '#FFF',
-    fontSize: 15,
-    lineHeight: 25,
+  vazamentoDescription: {
+    fontSize: 16,
+    marginBottom: 5,
   },
- 
-  logoutIcon: {
-    width: 50,
-    height: 50,
+  deleteButton: {
+    color: 'red',
+  },
+  editButton: {
+    color: 'blue',
+    marginRight: 10,
+  },
+  formContainer: {
     marginBottom: 20,
+  },
+  input: {
+    backgroundColor: '#FFF',
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+    width: '100%',
+ 
+
   },
 });
 
